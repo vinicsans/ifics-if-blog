ef9a9b394ac4f1827e6ea8947c01821dcd0ac06d
Vinicius Santos <87083914+vinicsans@users.noreply.github.com>
2023-04-17 20:39:50 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,6 @@ export default function Footer() {
       <div className="my-8 flex items-center">
         <div className="ml-0 mr-auto flex items-center justify-center space-x-4 leading-12">
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
         </div>
         <div className="text-md ml-auto mr-0 flex leading-12 text-amber-400 max-sm:text-xs">
           {"Feito com <3 por Marina Santos e Vinicius Alves"}
