4ccb279b7f6e4a84101bdb94ba8d8bf9ea71ab5f
Vinicius Santos <87083914+vinicsans@users.noreply.github.com>
2023-04-17 20:41:35 -0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -14,11 +14,7 @@ const siteMetadata = {
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/vinicsans/ifics-if-blog',
   locale: 'pt-BR',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
